82999609cdc7bd77f800319719dc58a09d6e599a
mayosuke <mayo.suke@gmail.com>
2014-03-08 17:48:59 +0900

    change strategy

--- players/our_player.rb
+++ players/our_player.rb
@@ -2,6 +2,7 @@ class OurPlayer < RpsTournament::Player
   def initialize
     @choice = RpsTournament::Game::CHOICES.sample
     @strategy = BeforeWinStrategy.new
+    @results = []
   end
 
   def choose
@@ -9,7 +10,18 @@ class OurPlayer < RpsTournament::Player
   end
 
   def result(my_choice, opponent_choice, result)
+    @results << result
+    @strategy = BeforeLoseStrategy.new if need_change_strategy?
+
     @choice = @strategy.calc(opponent_choice, result)
+  end
+
+  def need_change_strategy?
+    latest_result = @results.last(5)
+    @results.size == 5 && (
+    @results.last(5).uniq == [:draw] ||
+      @results.last(2) == [:lose, :win]
+    )
   end
 end
 
@@ -22,6 +34,19 @@ class BeforeWinStrategy
       :scissors
     when :scissors
       :rock
+    end
+  end
+end
+
+class BeforeLoseStrategy
+  def calc(opponent_choice, result)
+    case opponent_choice
+    when :rock
+      :scissors
+    when :paper
+      :rock
+    when :scissors
+      :paper
     end
   end
 end
